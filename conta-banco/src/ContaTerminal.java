61f8b01af55a8f3d4f0dad65f6c2dc4a5db7996e
Haryston <harystondaniel123@gmail.com>
2024-05-20 23:11:02 -0300

    projeto concluído

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+       System.out.println("Por favor, digite seu Nome: ");
+       String NomeCliente = scanner.next();
+
+       System.out.println("Por favor, digite o numero da conta: ");
+       int numeroConta = scanner.nextInt();
+
+       System.out.println("Por favor, digite o numero da Agência: ");
+       String Agencia = scanner.next();
+
+       
+       System.out.println("Por favor, digite o seu saldo: ");
+       double saldo = scanner.nextDouble();
+
+       
+        System.out.println("Olá, " +  NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ Agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
---
+++ conta-banco/src/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+       System.out.println("Por favor, digite o numero da Agência: ");
